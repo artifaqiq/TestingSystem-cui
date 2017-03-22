f91a08e5d114122e785b072f85b9ca9ae7d6c21d
Artur Lomako <artifaqiq@gmail.com>
2017-03-22 18:47:56 +0300

    Created javadoc for comments

--- src/main/java/com/nc/dev3/lomako/enums/CategorySortWays.java
+++ src/main/java/com/nc/dev3/lomako/enums/CategorySortWays.java
@@ -1,8 +1,13 @@
+/**
+ * Copyright (c) 2017, Lomako. All rights reserved.
+ */
 package com.nc.dev3.lomako.enums;
 
 /**
- * Created by arturlomako on 3/19/17.
- */
+ * Show available kind of sort category entities
+ * <li>{@link #BY_NAME}</li>
+ * <li>{@link #BY_COUNT_TESTS}</li>
+ * */
 public enum CategorySortWays {
     BY_NAME, BY_COUNT_TESTS;
 }
--- src/main/java/com/nc/dev3/lomako/enums/ResultCalculationStrategyWays.java
+++ src/main/java/com/nc/dev3/lomako/enums/ResultCalculationStrategyWays.java
@@ -3,6 +3,26 @@ package com.nc.dev3.lomako.enums;
 /**
  * Created by arturlomako on 3/21/17.
  */
+
+/**
+ * Show available kind of calculate test result
+ *
+ * <li>{@link #STRICT}</li>
+ * <li>{@link #SCALED}</li>
+ * */
 public enum ResultCalculationStrategyWays {
-    STRICT, SCALED;
+
+    /**
+     * In this instance in a task
+     * that has multiple answers,
+     * the correct answers should be all
+     * */
+    STRICT,
+
+    /**
+     * In this instance in a task
+     * that has multiple answers,
+     * points are awarded for each correct answer
+     * */
+    SCALED;
 }
