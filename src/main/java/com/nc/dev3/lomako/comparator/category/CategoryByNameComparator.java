f82e00d6537e7641ab7b3680b4537d4a7e502813
Artur Lomako <artifaqiq@gmail.com>
2017-03-22 16:12:59 +0300

    Added javadoc for categories comparators

--- src/main/java/com/nc/dev3/lomako/comparator/category/CategoryByNameComparator.java
+++ src/main/java/com/nc/dev3/lomako/comparator/category/CategoryByNameComparator.java
@@ -1,3 +1,6 @@
+/**
+ * Copyright (c) 2017, Lomako. All rights reserved.
+ */
 package com.nc.dev3.lomako.comparator.category;
 
 import com.nc.dev3.lomako.beans.category.Category;
@@ -5,9 +8,20 @@ import com.nc.dev3.lomako.beans.category.Category;
 import java.util.Comparator;
 
 /**
- * Created by arturlomako on 3/19/17.
- */
+ * This class implement interface {@code Comparator}
+ * May be used for sort collections of categories by name
+ *
+ * @author lomako
+ * @version 1.0
+ * @see Comparator
+ * */
 public class CategoryByNameComparator implements Comparator<Category> {
+
+    /**
+     * Compare two categories by name
+     *
+     * @see Comparator#compare(Object, Object)
+     * */
     @Override
     public int compare(Category o1, Category o2) {
         return o1.getName().compareToIgnoreCase(o2.getName());
--- src/main/java/com/nc/dev3/lomako/comparator/category/CategoryComparatorCreator.java
+++ src/main/java/com/nc/dev3/lomako/comparator/category/CategoryComparatorCreator.java
@@ -1,3 +1,6 @@
+/**
+ * Copyright (c) 2017, Lomako. All rights reserved.
+ */
 package com.nc.dev3.lomako.comparator.category;
 
 import com.nc.dev3.lomako.beans.category.Category;
@@ -6,12 +9,26 @@ import com.nc.dev3.lomako.enums.CategorySortWays;
 import java.util.Comparator;
 
 /**
- * Created by arturlomako on 3/19/17.
+ * This class is factory for generating strategies comparators
+ *
+ * @author lomako
+ * @version 1.0
+ *
+ * @see CategoryByCountTestsComparator
+ * @see CategoryByNameComparator
+ * @see CategorySortWays
  */
 public class CategoryComparatorCreator {
 
+    /**
+     * closed constructor
+     * */
     private CategoryComparatorCreator() { }
 
+    /**
+     * @param way way for compare two categories
+     * @return implementation of {@code Comparator<Strategy>}
+     * */
     public static Comparator<Category> create(CategorySortWays way) {
         Comparator<Category> result = null;
 
